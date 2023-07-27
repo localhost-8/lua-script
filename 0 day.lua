750978949e9a3109f77cc0e059947f3631b72754
staffuser <119116574+staffuser@users.noreply.github.com>
2023-07-27 17:03:49 +0300

    Update 0 day.lua

--- 0 day.lua
+++ 0 day.lua
@@ -1,8 +1,24 @@
 print("Start maker coffee")
-
-coffee = io.read()
+print("Import number - 1, 2, 3, 4")
+coffee = tonumber(io.read())
 
 function MakeCoffee(whatCoffee)
   print("switch Function" .. whatCoffee)
   print("Object" .. whatCoffee .. "complite")
+end
+
+if coffee == 1 then
+  Makecoffee(" 1 - Milk shake")
+end 
+
+if coffee == 2 then
+  Makecoffee(" 2 - Classic coffee")
+end 
+
+if coffee == 3 then
+  Makecoffee(" 3 - Water")
+end
+
+if coffee == 4 then
+  Makecoffee(" 4 - Milk")
 end
